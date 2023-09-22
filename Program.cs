7433919e342f2082f6478ca0acbbe5977f138d45
Kaleb Rionda <sergiokaleb.riondamorales@gmail.com>
2023-09-22 12:59:09 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace MultidimensionalesG2_2024_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,,] miArregloCaracteres = { { { 'a', 'b', 'c' }, { 'd', 'e', 'f' } }, { { 'g', 'h', 'i' }, { 'j', 'k', 'l' } } };
+            Console.WriteLine(miArregloCaracteres[1,1,1]);
+            Console.ReadLine();
+        }
+    }
+}
